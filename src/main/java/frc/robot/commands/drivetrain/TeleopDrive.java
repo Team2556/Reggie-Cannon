9c2b560aba0b12bde7e104b5faadce9853de2600
Chase Robbins <chasearobbins@icloud.com>
2024-12-05 16:07:53 -0600

    Initial Commit

---
+++ src/main/java/frc/robot/commands/drivetrain/TeleopDrive.java
@@ -0,0 +1,66 @@
+package frc.robot.commands.drivetrain;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+
+import java.util.function.Supplier;
+
+public class TeleopDrive extends Command {
+
+    Drivetrain drivetrain = Drivetrain.getInstance();
+
+    private final Supplier<Double> forward, strafe, turn;
+    private final Supplier<Boolean> shoot;
+    // private Timer timer;
+
+    public TeleopDrive(Supplier<Double> forward, Supplier<Double> strafe, Supplier<Double> turn, Supplier<Boolean> shoot) {
+        this.forward = forward;
+        this.strafe = strafe;
+        this.turn = turn;
+        this.shoot = shoot;
+
+        addRequirements(drivetrain);
+        setName("TeleOp Drive");
+    }
+
+
+    @Override
+    public void initialize() {
+        drivetrain.pmCompressor.enableDigital();
+        // timer.start();
+    }
+
+    @Override
+    public void execute() {
+        drivetrain.driveTeleop(-forward.get() * 0.5, strafe.get() * 0.5, turn.get() * 0.5);
+
+        if(shoot.get()) {
+            // drivetrain.setRelay(Relay.Value.kOn);
+            drivetrain.setRelay(Relay.Value.kReverse);
+            // if (timer.get() >= 5000) {
+            // }
+        } else {
+            drivetrain.setRelay(Relay.Value.kOff);
+        }
+
+        // if (drivetrain.pmCompressor.getPressure() > 60) {
+        //     drivetrain.pmCompressor.disable();
+        // } else {
+        //     drivetrain.pmCompressor.enableDigital();
+        // }
+           
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.driveTeleop(0.0,0.0,0.0);
+        drivetrain.setRelay(Relay.Value.kOff);
+        drivetrain.pmCompressor.disable();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
